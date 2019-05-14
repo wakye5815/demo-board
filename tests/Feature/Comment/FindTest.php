11771aef09b11424a448d9ccf42485e4915aa18d
wakye5815 <wakye5815@gmail.com>
2019-05-15 00:05:56 +0900

    CommentAPIのテストコード実装

---
+++ tests/Feature/Comment/FindTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Feature\Comment;
+
+use Tests\TestCaseRequireUser;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Http\ResponseBuilders\SuccessResponseBuilder;
+use App\Board;
+use App\Comment;
+
+class FindTest extends TestCaseRequireUser
+{
+    use RefreshDatabase;
+
+    /**
+     * @var Board
+     */
+    private $board = null;
+
+    /**
+     * @var Comment
+     */
+    private $comment = null;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->board = factory(Board::class)->create([
+            'owner_user_id' => $this->user->id
+        ]);
+        $this->comment = factory(Comment::class)->create([
+            'owner_user_id' => $this->user->id,
+            'board_id' => $this->board->id
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function 一つの任意のコメントを取得する()
+    {
+        $params = ['comment_id' => $this->comment->id];
+
+        $expectedResponse = (new SuccessResponseBuilder())
+            ->setContent(['comment' => [
+                'owner_name' => $this->user->name,
+                'board_id' => $this->board->id,
+                'content' => $this->comment->content
+            ]])
+            ->toArray();
+
+        $this->actingAs($this->user)
+            ->json('GET', '/api/comment/find', $params)
+            ->assertStatus(200)
+            ->assertJsonCount(1, 'content')
+            ->assertJson($expectedResponse);
+    }
+}
